ed5ffd52a69265e980c427df1052ebc3674b857b
Jordan MacBook <suarez.jordan@orange.fr>
2021-04-22 13:30:49 +0200

    chore

--- routes/user.js
+++ routes/user.js
@@ -17,21 +17,4 @@ userRouter.put('/update/me', updateMyProfile);
 // Delete me
 userRouter.delete('/delete/me', deleteMyProfile);
 
-// TODO only for admin
-// Update user
-// userRouter.put('/users/:id', passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     updateUser(req, res);
-//   });
-// Delete user
-// userRouter.delete('/users/:id', passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     deleteUser(req, res);
-//   });
-// Create User
-// userRouter.post('/users/new', passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     userController.createUser(req, res);
-//   });
-
 module.exports = userRouter;
